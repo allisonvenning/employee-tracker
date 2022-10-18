a515f2d4980557e3bf17d3f32b86853a999a16e7
Allison Venning <allisonvenning7@gmail.com>
2022-10-17 22:06:57 -0400

    added methods

--- db/index.js
+++ db/index.js
@@ -8,24 +8,23 @@ class DB {
         return this.connection.promise().query("SELECT department.id, department.name FROM department;");
     }
     showRoles() {
-        return this.connection.promise().query("SELECT role.id, role.name FROM role;");
+        return this.connection.promise().query("SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id = department.id;");
     }
     showEmployees() {
-        return this.connection.promise().query("SELECT employee.id, employee.name FROM employee;");
+        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONTACT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;");
     }
-    createDepartment() {
-        return this.connection.promise().query("INSERT INTO department;");
+    createDepartment(department) {
+        return this.connection.promise().query("INSERT INTO department SET ?", department);
     }
-    createRole() {
-
+    createRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
     }
-    createEmployee() {
-
+    createEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
     }
-    editEmployee() {
-
-    }
-    quit() {
-        
+    editEmployee(employeeId, roleId) {
+        return this.connection.promise().query("UPDATE employee SET role_id = ? WHERE id = ?", [employeeId, roleId]);
     }
 }
+
+module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -97,8 +97,59 @@ function addRole(role) {
     
 }
 
-function addEmployee(employee) {
-    
+function addEmployee() {
+    prompt([ 
+        {
+            name: "first_name",
+            message: "What is their first name?"
+        },
+        {
+            name: "last_name",
+            message: "What is their last name?"
+        }
+    ]).then(res => {
+        let firstName = res.first_name; 
+        let lastName = res.last_name;
+        db.showRoles()
+        .then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({id, title}) => ({
+                name: title, 
+                value: id
+            }));
+            prompt({
+                type: "list",
+                name: "roleId",
+                message: "What is the employee's role?",
+                choices: roleChoices
+            })
+            .then(res => {
+                let roleId = res.roleId;
+                db.showEmployees()
+                .then(([rows]) => {
+                    let employees = rows;
+                    const managerChoices = employees.map(({id, first_name, last_name}) => ({
+                        name: `${first_name} ${last_name}`,
+                        value: id
+                    }));
+                    managerChoices.unshift({name: "None", value: "Null"});
+                    prompt({
+                        type: "list",
+                        name: "managerId",
+                        message: "Who is the employee's manager?",
+                        choices: managerChoices
+                    }).then(res => {
+                        let employee = {
+                            // manager id, role id
+                            manager_id: res.managerId
+                        }
+                        db.createEmployee(employee)
+                    })
+                    .then(() => init())
+                    })
+                })
+            })
+        })
 }
 
 function updateEmployee(employeeId) {
